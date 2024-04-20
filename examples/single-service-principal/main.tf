69ffa32b9878493d6787f5d3ea5e4e1611754bf1
Colin Loh <colin.loh@afca.org.au>
2024-04-21 00:03:24 +1000

    fix example folder

--- examples/single-service-principal/main.tf
+++ examples/single-service-principal/main.tf
@@ -2,8 +2,13 @@ module "service_principal" {
   source = "../.."
 
   service_principal = {
-    name       =  "SPN-WITH-NO-GRAPH"
+    name       =  "SPN-ONE"
     permissions = [
+      {
+        Permission = "MicrosoftGraph",
+        Role       = ["User.ReadWrite.All", "User.Read.All"]
+        Scope      = ["User.ReadWrite"]
+      },
       {
         Permission = "DynamicsCrm",
         Scope      = ["user_impersonation"]
